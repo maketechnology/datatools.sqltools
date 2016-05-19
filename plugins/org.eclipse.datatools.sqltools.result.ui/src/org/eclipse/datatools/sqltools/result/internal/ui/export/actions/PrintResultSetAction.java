4089a266c4df3dd9c6d2e4a11f8f56fe8d7e8427
Emiliano <Emiliano@Emiliano-VAIO>
2016-05-19 17:42:51 -0300

    US069: -Not found OSGI service registered in dialogs
    https://podio.com/we-make-technology/dbtondtp/apps/stories/items/69

--- plugins/org.eclipse.datatools.sqltools.result.ui/src/org/eclipse/datatools/sqltools/result/internal/ui/export/actions/PrintResultSetAction.java
+++ plugins/org.eclipse.datatools.sqltools.result.ui/src/org/eclipse/datatools/sqltools/result/internal/ui/export/actions/PrintResultSetAction.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.datatools.sqltools.result.internal.ui.export.actions;
 
-import javax.swing.text.StyleContext;
-
 import org.eclipse.datatools.help.HelpUtil;
 import org.eclipse.datatools.sqltools.result.IResultSetObject;
 import org.eclipse.datatools.sqltools.result.internal.export.TextOutputter;
@@ -20,80 +18,69 @@ import org.eclipse.datatools.sqltools.result.model.IResultInstance;
 import org.eclipse.datatools.sqltools.result.ui.IHelpConstants;
 import org.eclipse.datatools.sqltools.result.ui.ResultsViewUIPlugin;
 import org.eclipse.jface.action.Action;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.printing.PrintDialog;
-import org.eclipse.swt.printing.Printer;
-import org.eclipse.swt.printing.PrinterData;
+// import org.eclipse.swt.printing.PrintDialog;
+// import org.eclipse.swt.printing.Printer;
+// import org.eclipse.swt.printing.PrinterData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * Print the result set object
- * 
+ *
  * @author Dafan Yang
  */
 public class PrintResultSetAction extends Action
 {
-    IResultSetObject            _result;
-    IResultInstance             _resultInstance;
-    Shell                       _shell;
-    Composite                   _parent;
+    IResultSetObject _result;
+    IResultInstance _resultInstance;
+    Shell _shell;
+    Composite _parent;
     private static final String FONT_STYLE = "Courier New";
-    private static final int    FONT_SIZE  = 10;
-    
-    public PrintResultSetAction(IResultSetObject result, Composite parent)
-    {
-        super(Messages.ResultSetAction_Title); 
+    private static final int FONT_SIZE = 10;
+
+    public PrintResultSetAction(IResultSetObject result, Composite parent) {
+        super(Messages.ResultSetAction_Title);
         this._shell = parent.getShell();
         _result = result;
         _parent = parent;
-        
+
         PlatformUI.getWorkbench().getHelpSystem().setHelp(this, HelpUtil.getContextId(IHelpConstants.ACTION_PRINT_RESULTSET, ResultsViewUIPlugin.getDefault().getBundle().getSymbolicName()));
     }
-    
-    public PrintResultSetAction(IResultInstance resultInstance, Composite parent)
-    {
-        super(Messages.AllResultSetAction_Title); 
+
+    public PrintResultSetAction(IResultInstance resultInstance, Composite parent) {
+        super(Messages.AllResultSetAction_Title);
         this._shell = parent.getShell();
         _resultInstance = resultInstance;
         _parent = parent;
     }
 
-    public void run()
-    {
-        PrintDialog pdlg = new PrintDialog(_shell, SWT.NONE);
-        PrinterData data = pdlg.open();
-        if(data != null)
+    public void run() {
+        // PrintDialog pdlg = new PrintDialog(_shell, SWT.NONE);
+        // PrinterData data = pdlg.open();
+        // if(data != null)
         {
-            Printer printer = new Printer(data);
-            //StyledText styledText = new StyleContext(_parent, SWT.NONE);
-            //styledText.setVisible(false);
+            // Printer printer = new Printer(data);
+            // StyledText styledText = new StyleContext(_parent, SWT.NONE);
+            // styledText.setVisible(false);
             TextOutputter outputter = new TextOutputter();
             String printString = "";
-            if(_result != null)
-            {
+            if (_result != null) {
                 printString = outputter.getPrintString(_result);
-            }
-            else if(_resultInstance != null)
-            {
+            } else if (_resultInstance != null) {
                 printString = outputter.getPrintString(_resultInstance);
-            }
-            else
-            {
+            } else {
                 // should not happen
-                printer.dispose();
+                // printer.dispose();
                 return;
             }
-            //FontData fd = new FontData(FONT_STYLE, FONT_SIZE, SWT.NORMAL);
-            //Font font = new Font(styledText.getDisplay(), fd);
-            //styledText.setFont(font);
-            //styledText.setText(printString);
-            //styledText.print(printer).run();
-            printer.dispose();
-            //font.dispose();
+            // FontData fd = new FontData(FONT_STYLE, FONT_SIZE, SWT.NORMAL);
+            // Font font = new Font(styledText.getDisplay(), fd);
+            // styledText.setFont(font);
+            // styledText.setText(printString);
+            // styledText.print(printer).run();
+            // printer.dispose();
+            // font.dispose();
         }
     }
 }
